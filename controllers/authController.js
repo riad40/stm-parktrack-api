ddb23432e37f033a80bc4c647d88d381474358e0
riad40 <riadabdelaziz40@gmail.com>
2023-03-24 23:39:52 +0000

    make the register functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User")
+const Role = require("../models/Role")
+const bcrypt = require("bcryptjs")
+
 // login function
 const login = async (req, res) => {
     res.send("login")
@@ -5,7 +9,38 @@ const login = async (req, res) => {
 
 // register function
 const register = async (req, res) => {
-    res.send("register")
+    // get the user data from the request body
+    const { username, email, password } = req.body
+
+    try {
+        // check if the user already exists
+        const userExist = await User.findOne({ email })
+        if (userExist) {
+            return res.status(400).json({ message: "user already exists" })
+        }
+
+        // get the user role id
+        const userRole = await Role.findOne({ name: "user" })
+
+        // hash the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // create the user
+        const user = new User({
+            username,
+            email,
+            password: hashedPassword,
+            role: userRole._id,
+        })
+        await user.save()
+
+        // send the response
+        res.status(201).json({ message: "user created successfully" })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "internal server error" })
+    }
 }
 
 // logout function
