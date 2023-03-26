02327d72da1919fb61dcc6a035f427716769cb20
riad40 <riadabdelaziz40@gmail.com>
2023-03-26 11:57:00 +0000

    check only if password exist and make other validation stuff on the frontend

--- middlewares/bodyValidator.js
+++ middlewares/bodyValidator.js
@@ -5,17 +5,14 @@ const validate = (method) => {
     switch (method) {
         // validate the login request body
         case "login": {
-            return [
-                body("email").exists().isEmail(),
-                body("password").isLength({ min: 4 }),
-            ]
+            return [body("email").exists().isEmail(), body("password").exists()]
         }
         // validate the register request body
         case "register": {
             return [
                 body("username").exists(),
                 body("email").exists().isEmail(),
-                body("password").isLength({ min: 4 }),
+                body("password").exists(),
             ]
         }
         // validate add log request body
